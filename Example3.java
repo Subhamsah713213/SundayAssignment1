0d982c8a5332d39cf59255e4f315d8de9378eb86
Subhamsah713213 <subhamsah713213@gmail.com>
2016-12-18 12:10:04 +0530

    Add files via upload

---
+++ Example3.java
@@ -0,0 +1,25 @@
+package com.Subham;
+
+public class Example3 {
+	public static void main(String args[])
+	{
+		int  number = 9;
+	    int space = number;
+	    for (int i = 1; i <= number; i++) {
+
+	        for(int s = 1; s <= space; s++) {
+	            System.out.print(" ");
+	        }
+	        space--;
+	        for (int j = 0; j < i; j++) {
+	            System.out.print(j);
+	        }
+	        for (int p = i - 2 ; p >= 0; p--) {
+	            System.out.print(p);
+	        }
+	        System.out.print("\n");
+	    }
+	  
+	}
+
+}
