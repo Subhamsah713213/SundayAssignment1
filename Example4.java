f41a5de84abac922dd982691a9167e39e9b29002
Subhamsah713213 <subhamsah713213@gmail.com>
2016-12-18 12:09:33 +0530

    Add files via upload

---
+++ Example4.java
@@ -0,0 +1,39 @@
+package com.Subham;
+import java.util.*;
+public class Example4 {
+	
+	
+	
+	public static void main(String args[])
+	
+	{
+	    int roll,x=1;
+	    String name,blood_group,y;
+	    Scanner sc=new Scanner(System.in);
+	    while(x==1)
+	    {
+	     System.out.println("enter the name");
+	     name=sc.next();
+	     System.out.println("enter roll");
+	     roll=sc.nextInt();
+	     System.out.println("enter blood_group");
+	     blood_group=sc.next();
+	     
+	     System.out.println("Name: "+name);
+	     System.out.println("Roll: "+roll);
+	     System.out.println("blood_group: "+blood_group);
+	     
+	     System.out.println("enter 1 for more entries and 0 to exit");
+	    
+	     x=sc.nextInt();
+
+	    }
+	}
+	
+	
+
+	    }
+	
+	
+
+
